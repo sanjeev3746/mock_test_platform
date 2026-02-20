3c0a0e55161d4ffdb36edbe4afd10041ce67b43e
Sanjeev Kumar <sanjeev3746@gmail.com>
2026-02-20 19:36:31 +0530

    Fix root route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/exam/:attemptId" element={<ExamPage />} />
         <Route path="/result/:attemptId" element={<ResultPage />} />
       </Routes>
